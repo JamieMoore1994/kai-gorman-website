5b2abc27da2d263bedf0288a44e7fe1d040732f4
Jamie Moore <17076460@stu.mmu.ac.uk>
2020-02-18 00:40:33 +0000

    Adding animation sequence to intro sections
    Adding short timeline animation sequence to the navigation and banner areas to each page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-
+// Mobile navigation
 const responsiveNav = function() {
     let nav = document.querySelector('.master__header--nav');
     let burger = document.querySelector('.master__header--burger');
@@ -13,3 +13,35 @@ const responsiveNav = function() {
 responsiveNav();
 
 
+// Animating the navigation area
+function introScene() {
+    var logo = document.querySelector('.logo');
+    var nav = document.querySelector(".master__header--links");
+    var navLinks = document.querySelectorAll(".master__header--links li");
+
+    var introWrapper = document.querySelector(".page__header--wrapper");
+    var bannerImg = document.querySelector(".page__header--image");
+
+
+    var tl = gsap.timeline({defaults:{opacity: 0}});
+        tl.from(logo, {xPercent: -100});
+        tl.from(nav, {}, "<")
+        tl.from(navLinks, {duration: 1, stagger: 0.1, ease: "bounce"}, "<")
+        tl.from(introWrapper, {xPercent: -100, ease: "back", duration: 1.5}, "-=1")
+        tl.from(bannerImg, {xPercent: 100, duration: 1}, "-=0.8");
+
+};
+
+// Fixing the issue of page rendering before the animation begins
+window.addEventListener("load", function(e) {
+    gsap.set(".master__header", {
+        autoAlpha: 1
+    });
+    gsap.set(".page__header", {
+        autoAlpha: 1
+    });
+    gsap.set(".page__header--wrapper", {
+        autoAlpha: 1
+    });
+    introScene();
+});
