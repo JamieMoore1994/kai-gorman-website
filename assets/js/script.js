e189b62bc527d93a50b96c733d3b5d9ecd9479aa
Jamie Moore <17076460@stu.mmu.ac.uk>
2020-02-20 12:29:26 +0000

    Fixing alignment issues
    Also shortening the timing for the intro animation timeline.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 // Mobile navigation
 const responsiveNav = function() {
-    let nav = document.querySelector('.master__header--nav');
-    let burger = document.querySelector('.master__header--burger');
-    let body = document.querySelector('body');
+    var nav = document.querySelector('.master__header--nav');
+    var burger = document.querySelector('.master__header--burger');
+    var body = document.querySelector('body');
 
     burger.addEventListener('click', function() {
         body.classList.toggle('body-toggle');
@@ -10,26 +10,22 @@ const responsiveNav = function() {
     });
 }
 
-responsiveNav();
-
-
 // Animating the navigation area
 function introScene() {
     var logo = document.querySelector('.logo');
-    var nav = document.querySelector(".master__header--nav");
-    var navLinks = document.querySelectorAll(".master__header--links li");
+    var navLinks = document.querySelector(".master__header--links");
+    var burgerDiv = document.querySelector('.master__header--burger');
 
-    var introWrapper = document.querySelector(".page__header--wrapper");
-    var bannerImg = document.querySelector(".page__header--image");
+    let introWrapper = document.querySelector(".page__header--wrapper");
+    let bannerImg = document.querySelector(".page__header--image");
 
 
-    var tl = gsap.timeline({defaults:{opacity: 0}});
-        tl.from(logo, {xPercent: -100});
-        tl.from(nav, {}, "<")
-        tl.from(navLinks, {duration: 1, stagger: 0.2, ease: "bounce"}, "<")
-        tl.from(introWrapper, {xPercent: -100, ease: "back", duration: 1.5}, "-=0.4")
-        tl.from(bannerImg, {xPercent: 100, duration: 1}, "-=0.8");
-
+    var tl = gsap.timeline({defaults:{opacity: 0, ease: "back"}});
+        tl.from(logo, {yPercent: -100, duration: 0.5});
+        tl.from(burgerDiv, {yPercent: -100, duration: 0.5}, "<");
+        tl.from(navLinks, {yPercent: -100, duration: 0.5}, "<")
+        tl.from(introWrapper, {xPercent: -100, duration: 1}, "-=0.4")
+        tl.from(bannerImg, {xPercent: 100, duration: 1}, "-=0.5");
 };
 
 // Fixing the issue of page rendering before the animation begins
@@ -43,5 +39,9 @@ window.addEventListener("load", function(e) {
     gsap.set(".page__header--wrapper", {
         autoAlpha: 1
     });
+    gsap.set(".master__header--links", {
+        autoAlpha: 1
+    });
+    responsiveNav();
     introScene();
 });
